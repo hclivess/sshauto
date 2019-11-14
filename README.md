847dd2e744d67369706a6a9d3582ea9da2cb3bd2
HCLivess <admin@bismuth.cz>
2019-11-14 16:21:03 +0100

    add linux support

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Automate SSH connections, send bulk commands to multiple servers without having to log in to them individually.
 
 Create `secret.json` from `secret_demo.json` and adjust settings.
+
+For linux, you need to `apt-get install plink`. For windows, you need the bundled `plink.exe`
--- sshauto.py
+++ sshauto.py
@@ -2,6 +2,8 @@ import sys
 import os
 import subprocess
 import json
+import platform
+
 
 pathname = os.path.abspath(os.path.dirname(sys.argv[0]))
 
@@ -15,8 +17,15 @@ def load_data():
         data = json.loads(secret_file.read())
     return data
 
+def command_determine():
+    if "Windows" in platform.system():
+        return "plink"
+    else:
+        return "./plink"
+
 if __name__ == "__main__":
     d = load_data()
+    os_exec = command_determine()
 
     for entry in d:
 
@@ -28,7 +37,7 @@ if __name__ == "__main__":
         for command in commands:
             print(command)
             print(f"Working on {ip}, {command}")
-            command_line = f"{pathname}\\plink {ip} -l root -pw {password} -m {pathname}\\{command}"
+            command_line = f"{pathname}/{os_exec} {ip} -l root -pw {password} -m {pathname}/{command}"
             pipe = subprocess.Popen(command_line, shell=True, stdout=subprocess.PIPE).stdout
             output = pipe.read().decode()
             pipe.close()
