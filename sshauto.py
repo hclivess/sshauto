9a23c897905d513059e8b38287a9d55c13c3778c
HCLivess <admin@bismuth.cz>
2019-11-28 20:59:46 +0100

    proper logging

--- sshauto.py
+++ sshauto.py
@@ -2,8 +2,8 @@ import subprocess
 import json
 import argparse
 from getpass import getpass as gp
-import logging
-from logging.handlers import RotatingFileHandler
+import logging.handlers
+
 
 def load_data(file="demo.json"):
 
@@ -17,13 +17,19 @@ if __name__ == "__main__":
     password = None
     parser = argparse.ArgumentParser()
 
+
     parser.add_argument("--conf", help="Server configuration file")
     args = parser.parse_args()
-    print(f"Selected configuration file: {args.conf}")
 
-    logger = logging.getLogger('my_logger')
-    handler = RotatingFileHandler(f'{args.conf}', maxBytes=2000, backupCount=10)
-    logger.addHandler(handler)
+    logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+    rootLogger = logging.getLogger()
+    rootLogger.setLevel(logging.INFO)
+    fileHandler = logging.FileHandler(f"log.log")
+    fileHandler.setFormatter(logFormatter)
+    rootLogger.addHandler(fileHandler)
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter(logFormatter)
+    rootLogger.addHandler(consoleHandler)
 
     d = load_data(args.conf)
 
@@ -47,12 +53,17 @@ if __name__ == "__main__":
         commands = entry["scripts"]
 
         for command in commands:
-            print(command)
-            print(f"Working on {ip}, {command}")
-            command_line = f"plink {ip} -l root -pw {password} -m {command} -batch"
-            pipe = subprocess.Popen(
-                command_line, shell=True, stdout=subprocess.PIPE
-            ).stdout
-            output = pipe.read().decode()
-            pipe.close()
-            logger.warning(output)
+
+            try:
+                rootLogger.info(command)
+                rootLogger.info(f"Working on {ip}, {command}")
+                command_line = f"plink {ip} -l root -pw {password} -m {command} -batch"
+                pipe = subprocess.Popen(
+                    command_line, shell=True, stdout=subprocess.PIPE
+                ).stdout
+                output = pipe.read().decode()
+                pipe.close()
+                rootLogger.info(output)
+
+            except Exception as e:
+                rootLogger.info(f"Error: {e}")
